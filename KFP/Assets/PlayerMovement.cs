e73cf1f9263a2e7e54719150e319bca06fad0cbf
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2019-04-14 01:10:30 -0500

    going to be experimenting with super mario run style automatic movement - added basic stuff to play around with

---
+++ KFP/Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script in charge of moving and jumping the player
+/* Player moves automatically forward unless told not to
+ * */
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool facingRight;
+    bool movingForward;
+    bool inAir;
+
+    private void Start()
+    {
+        
+    }
+
+
+    //Method called to make the player jump
+    public void jump()
+    {
+        //jumpPress = true;
+
+        if (!inAir)
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 3000);
+        }
+    }
+
+
+    //Method checks if the player is in midair by testing y velocity speeds
+    void MidairCheck()
+    {
+        if (!inAir && Mathf.Abs(this.GetComponent<Rigidbody2D>().velocity.y) > 0.05f)
+        {
+            inAir = true;
+        }
+        else if (inAir && this.GetComponent<Rigidbody2D>().velocity.y == 0.0f)
+        {
+            inAir = false;
+            //if (jumpPress) jump();
+        }
+    }
+
+
+    // Switch the direction the player is facing
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
