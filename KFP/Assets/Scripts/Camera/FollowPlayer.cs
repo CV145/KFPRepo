8d868c4cd92440ce15ea4ec45623eb85920d4626
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2019-04-27 21:16:20 -0500

    Carlos - refactored scripts, created a class that stores timelines for camera

---
+++ KFP/Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//script follows the player until a timeline is triggered
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+    [SerializeField] bool focusOnPlayer;
+
+    private void Update()
+    {
+        moveToPlayer();
+    }
+
+    //always follow the player
+    private void moveToPlayer()
+    {
+        if (focusOnPlayer)
+        {
+            transform.position = new Vector3(playerPosition.position.x + 13, transform.position.y, -10f);
+        }
+    }
+}
---
+++ KFP/Assets/Scripts/Player/ShootRaycastDetector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* GameObject: Player
+- Has methods that take in a RaycastHit and can be called to trigger events on objects that were hit, for example reducing hits on an object or triggering a switch */
+
+public class ShootRaycastDetector : MonoBehaviour
+{
+    //checks information retrieved by the raycast hit and calls methods accordingly
+    public void DetectRaycastHit(RaycastHit2D hit)
+    {
+        string tag = hit.transform.gameObject.tag;
+        GameObject collidedObject = hit.transform.gameObject;
+        GameObject collidedObjectParent = collidedObject.transform.parent.gameObject;
+
+        switch (tag)
+        {
+            case "Enemy":
+                if (collidedObjectParent.GetComponent<DestructibleObject>())
+                {
+                    collidedObjectParent.GetComponent<DestructibleObject>().TakeDamage();
+                }
+                break;
+        }
+    }
+}
