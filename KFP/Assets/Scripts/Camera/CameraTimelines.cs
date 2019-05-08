4ce8079b2e75b3e421c9f9863b90b18287b6732a
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2019-05-07 21:35:11 -0500

    Carlos - working on camera movement

--- KFP/Assets/Scripts/Camera/CameraTimelines.cs
+++ KFP/Assets/Scripts/Camera/CameraTimelines.cs
@@ -8,7 +8,6 @@ using UnityEngine.Timeline;
 [RequireComponent(typeof(PlayableDirector))]
 public class CameraTimelines : MonoBehaviour
 {
-    //[SerializeField] List<PlayableDirector> playableDirectors; //can one director work instead?
     PlayableDirector director;
     [SerializeField] List<TimelineAsset> timelines;
     Dictionary<string, TimelineAsset> timelineDictionary = new Dictionary<string, TimelineAsset>();
@@ -28,26 +27,12 @@ public class CameraTimelines : MonoBehaviour
         timelineIsActive = true;
     }
 
-    private void Update()
-    {
-        TimelineStopPlayingCheck();
-    }
-
-    //puts all the timelines in the dictionary - timelines are accessed using their names
+    //puts all the timelines in the dictionary - timelines are accessed using their names 
     private void StoreTimelinesInDictionary()
     {
         foreach (TimelineAsset timeline in timelines)
         {
             timelineDictionary.Add(timeline.name, timeline);
-        }
-    }
-
-    //if a timeline is playing, check for when it stops playing
-    private void TimelineStopPlayingCheck()
-    {
-        if (timelineIsActive)
-        {
-            //if (director)
         }
     }
 }
