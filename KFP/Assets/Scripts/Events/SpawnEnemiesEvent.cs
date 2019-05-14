b37a27eb05892b7a595f14c8af3fe8e4933adfac
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2019-05-14 01:36:04 -0500

    Carlos - added an event for spawning enemies

---
+++ KFP/Assets/Scripts/Events/SpawnEnemiesEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//enables all selected enemies in enemiesToEnable[]
+public class SpawnEnemiesEvent : TriggerEvent
+{
+    [SerializeField] GameObject[] enemiesToEnable;
+    [Header("What to do beside spawning enemies")]
+    [SerializeField] UnityEvent spawnEvent;
+
+    private new void Start()
+    {
+        base.Start();
+        spawnEvent.AddListener(enableEnemies);
+    }
+
+    private void enableEnemies()
+    {
+        foreach (GameObject enemy in enemiesToEnable)
+        {
+            enemy.SetActive(true);
+        }
+    }
+
+    override
+    public void InvokeResponses()
+    {
+        spawnEvent.Invoke();
+    }
+}
