8646af366e84bbcc1de51afddf96efe4b7cc88e3
Joel <nintendogamer249@gmail.com>
2019-07-30 19:59:24 -0400

    Joel - Added Music To City
    Added a script the plays music and changes it when the Player enters an area with a different sky

---
+++ KFP/Assets/CityMusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityMusicController : MonoBehaviour
+{
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioSource source2;
+    void Start()
+    {
+        source.GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        source2.Stop();
+        source.Play();
+
+    }
+}
