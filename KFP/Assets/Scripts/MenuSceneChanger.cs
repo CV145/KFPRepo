eace106882d5ea0da366fafef00e50ef18d42ef7
CV145 <carlosvaleriano45@yahoo.com>
2019-09-25 00:04:29 -0500

    Carlos - split out the level segments, added level win event, added exit button for donut shop

---
+++ KFP/Assets/Scripts/MenuSceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script with methods for changing scenes. UI buttons only.
+/// </summary>
+public class MenuSceneChanger : MonoBehaviour
+{
+    public void ChangeScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
